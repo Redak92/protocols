774d193cee84a8541a4a558b804e751ee9a0abf7
Redak92 <134450370+Redak92@users.noreply.github.com>
2025-03-12 10:58:48 +0100

    update

--- IP/IP.py
+++ IP/IP.py
@@ -4,10 +4,12 @@ import socket
 class Socket():
     def __init__(self):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
-
+        self.mtu = 1500
+        self.ip = self.get_own_ip()
     def encapsulate_ip(self, destination: str, data: str):
         ip_version = 0x4
-
+        ihl = 0x5 # For now, we ignore options
+        
 
     def get_own_ip(self):
         hostname = socket.gethostname()
