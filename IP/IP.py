1b95d5a35c4a9406c261bae778c98935bf903528
Redak92 <134450370+Redak92@users.noreply.github.com>
2025-03-13 12:45:36 +0100

    update

--- IP/IP.py
+++ IP/IP.py
@@ -6,11 +6,22 @@ class Socket():
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
         self.mtu = 1500
         self.ip = self.get_own_ip()
-    def encapsulate_ip(self, destination: str, data: str):
+        self.packet_number = 0
+        self.ttl = 8
+    def encapsulate_ip(self, destination: str, data: str, protocol: int):
         ip_version = 0x4
         ihl = 0x5 # For now, we ignore options
+        dscp = 0x0  # Low priority DSCP 
+        data = data.encode()
+        total_length = len(data) + 4 * ihl
+        total_length_bytes = total_length.to_bytes(2, byteorder='big')
+        identification = self.packet_number.to_bytes(2, byteorder='big')
+        self.packet_number += 1
+        if self.packet_number > 65535:
+            self.packet_number = 0
+        number_of_packets = (total_length + self.mtu - 1) // self.mtu
+        protocol = protocol.to_bytes(1, byteorder='big')
         
-
     def get_own_ip(self):
         hostname = socket.gethostname()
         own_ip = socket.gethostbyname(hostname)
