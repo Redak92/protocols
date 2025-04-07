f22f5af3c4335a919f64357f123f7becb4a531f4
trash <trash@harmon.home>
2025-04-07 12:23:22 +0200

    update

--- IP/test.py
+++
@@ -1,5 +0,0 @@
-from UDP import SocketUDP
-
-
-s = SocketUDP("127.0.0.1")
-s.send_udp(("127.0.0.1", 8081), b"Hello from UDP client!")
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# protocols
+I'm going to use Scapy to rebuild the entire stack.
